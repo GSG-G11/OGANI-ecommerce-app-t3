b670105e9de73cc4d82552fadf8d98e8a6f4f65b
yousra Jaradh <yousrakhaleel@gmail.com>
2022-02-02 19:36:32 +0300

    add-button

---
+++ js/seller.js
@@ -0,0 +1,5 @@
+const buttons = document.querySelector(".add-product-seller");
+const forms = document.querySelector(".add-form-container");
+buttons.addEventListener("click", () => {
+  forms.style.display = "block";
+});
