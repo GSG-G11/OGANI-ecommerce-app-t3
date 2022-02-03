1b607600c4ee8813260e3f82eeda921ed4003707
zaher-aa <zaherabuamro@gmail.com>
2022-02-03 09:59:42 +0200

    Fixed Array Methods Relates #37

--- js/logic.js
+++ js/logic.js
@@ -1,39 +1,34 @@
 // Edit Product --Seller
 function editProduct(id, property, replaceValue, modifiedProducts) {
-    modifiedProducts.map((element) => {
-        if (element["id"] == id) {
-            element[property] = replaceValue;
-        }
-    });
-    return modifiedProducts;
+	return modifiedProducts.map((ele) => {
+		if (Number(ele.id) === Number(id)) {
+			ele[property] = replaceValue;
+		}
+		return ele;
+	});
 }
 
 function deleteProduct(id, products) {
-    products.filter((element, index) => {
-        if (element["id"] == id) {
-            products.splice(index, 1); // Get item index and Delete 1 item
-        }
-    });
-    return products;
+	return products.filter((ele) => id != ele.id);
 }
 
 function searchProduct(product, products) {
-    return products.filter((item) =>
-        item.productName.toLowerCase().includes(product.toLowerCase())
-    );
+	return products.filter((item) =>
+		item.productName.toLowerCase().includes(product.toLowerCase())
+	);
 }
 
 function filterProduct(category = "", price = "", products) {
-    const newProducts = [...products];
-    if (price === "low") newProducts.sort((a, b) => (a.price > b.price ? 1 : -1)); // sort price to low
-    if (price === "high")
-        newProducts.sort((a, b) => (a.price > b.price ? -1 : 1)); // sort price to High
-    return newProducts.filter((item) => item["category"] === category); // Sort category
+	const newProducts = [...products];
+	if (price === "low") newProducts.sort((a, b) => (a.price > b.price ? 1 : -1)); // sort price to low
+	if (price === "high")
+		newProducts.sort((a, b) => (a.price > b.price ? -1 : 1)); // sort price to High
+	return newProducts.filter((item) => item["category"] === category); // Sort category
 }
 
 module.exports = {
-    searchProduct,
-    filterProduct,
-    editProduct,
-    deleteProduct
+	searchProduct,
+	filterProduct,
+	editProduct,
+	deleteProduct,
 };
